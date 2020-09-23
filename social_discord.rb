84837639b71735c541c635d976cd102af122cde0
Zack Herbert <zackherbert+github@gmail.com>
2020-09-23 13:10:06 -0400

    tweak bot to function sucessfully and send tweet

--- social_discord.rb
+++ social_discord.rb
@@ -1,9 +1,8 @@
 require 'discordrb'
 require 'twitter'
 
-bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN'], client_id: ENV['DISCORD_CLIENT_ID'], ignore_bots: true
-
-bot.message(with_text: 'Going live!') do |event|
+bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN'], ignore_bots: true
+bot.message({in: ENV['CHANNEL_ID'], start_with: 'Going live!'}) do |event|
     client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV[TWITTER_CONSUMER_KEY]
         config.consumer_secret     = ENV[TWITTER_CONSUMER_SECRET]
@@ -11,7 +10,7 @@ bot.message(with_text: 'Going live!') do |event|
         config.access_token_secret = ENV[TWITTER_ACCESS_TOKEN_SECRET]
     end
 
-    client.update(bot.message.content)
+    client.update(event.message.content)
 end
 
 bot.run
