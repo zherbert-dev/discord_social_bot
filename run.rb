2c5ec0abe2b0930120d8e578a0aa8eff8c8a2308
Zack Herbert <zackherbert+github@gmail.com>
2020-09-27 01:24:32 -0400

    uncomment out code

--- run.rb
+++ run.rb
@@ -12,15 +12,15 @@ rc = Redis.new
 
 # Monitor a given channel for a message that starts with 'Going live!' and send 
 # the message contents to the configured Twitter account
-# bot.message({in: ENV['ANNOUNCEMENTS_CHANNEL_ID'], start_with: 'Going live!'}) do |event|
-#     twitter_client = set_twitter_client
-#     create_twitter_post(twitter_client, event)
-# end
+bot.message({in: ENV['ANNOUNCEMENTS_CHANNEL_ID'], start_with: 'Going live!'}) do |event|
+    twitter_client = set_twitter_client
+    create_twitter_post(twitter_client, event)
+end
 
 # ## Monitor main channel for a new user to join and respond with a welcome message
-# bot.member_join do |event|
-#     welcome_new_member(event)
-# end
+bot.member_join do |event|
+    welcome_new_member(event)
+end
 
 # Bot Commands and Response
 # Commands will be messages that start with '!'
