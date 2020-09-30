42aae5844b2ef24892672824704bc639d1a4fd54
Zack Herbert <zackherbert+github@gmail.com>
2020-09-30 11:24:05 -0400

    remove debug puts

--- modules/level_up_logic.rb
+++ modules/level_up_logic.rb
@@ -3,7 +3,6 @@
 module LevelUpLogic
   def self.add_points(r_conn, event)
     existing_user = get_existing_user(r_conn, event.user)
-    pp existing_user
     if !existing_user.nil?
       new_points = r_conn.hincrby(event.user.id.to_s, :points, 1)
       check_points(r_conn, new_points, event)
@@ -21,20 +20,17 @@ module LevelUpLogic
   end
 
   def self.check_points(r_conn, new_points, event)
-    puts "checking points: #{new_points}"
     return unless [5, 50, 100, 500, 1000, 5000, 10_000, 15_000, 50_000, 100_000].include? new_points
 
     level_up(r_conn, event.user, event)
   end
 
   def self.level_up(r_conn, user, event)
-    puts "level up"
     new_level = r_conn.hincrby(user.id.to_s, :level, 1)
     notify_level_up(event, new_level)
   end
 
   def self.notify_level_up(event, new_level)
-    puts "notifying"
     username = event.user.username
     begin
       event.channel.send_message("Congrats @#{username} -- you have increased to lvl #{new_level}!")
